6ac8ed1e3a8f80d173310451972b11176d063d26
Diego Duran <grupointi@grupointi.net>
2021-08-24 16:41:44 -0500

    make it better 2

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ app.use('/api/v1/tasks', tasks)
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/dist/angular-crash'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist/angular-crash', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client/dist', 'angular-crash', 'index.html')));
 }
 
 
 
-app.get('/', (req, res) => res.send('holaa'));
+app.get('/', (req, res) => res.send('hello'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
