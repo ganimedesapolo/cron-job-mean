b601bb053553e72b158d8bb50bfb78983bd16527
Diego Duran <grupointi@grupointi.net>
2021-08-24 16:24:51 -0500

    make it better 2

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/v1/tasks', tasks)
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/dist/angular-crash'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client/dist', 'angular-crash', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist/angular-crash', 'index.html')));
 }
 
 
