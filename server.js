c8acca6b5c6038565e7473c0736a4ebcd8537826
Diego Duran <grupointi@grupointi.net>
2021-08-24 15:39:17 -0500

    make it better 2

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ if(process.env.NODE_ENV==='production'){
 
 app.get('/', (req, res) => res.send('holaa'));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT ||8080
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
